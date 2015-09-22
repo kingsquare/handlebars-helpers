9c32666f0f1f7bced5bffa8dc4285da539083eb7
Tim de Koning <git@king-management.nl>
2015-09-22 12:53:18 +0200

    A better dateformatter

--- src/dateFormat.js
+++ src/dateFormat.js
@@ -7,5 +7,14 @@ module.exports = function (date, block) {
 	if (!date) {
 		return '';
 	}
-	return moment(date).tz('Europe/Amsterdam').format(block.hash.format || "DD-MM-YYYY");
+
+	var format =  "DD-MM-YYYY";
+	if (block && block.hash && block.hash.format) {
+		format = block.hash.format;
+	}
+	if (typeof block === "string") {
+		format = block;
+	}
+
+	return moment(date).tz('Europe/Amsterdam').format(format);
 };
