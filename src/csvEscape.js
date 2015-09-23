b989247ed41bd329abf2d4c7abd99304375b1380
Tim de Koning <git@king-management.nl>
2015-09-23 09:23:35 +0200

    Typehandling for  csvEscape bump 1.0.5

--- src/csvEscape.js
+++ src/csvEscape.js
@@ -2,15 +2,25 @@
 
 /**
  * Based on https://github.com/wdavidw/node-csv-stringify/blob/master/lib/index.js
- * 
- * @param string
+ *
+ * @param value
  * @returns {*}
  */
-module.exports = function(string) {
+module.exports = function(value) {
 	var delimiter = ";";
 	var escape = '"';
 	var quote = '"';
 
+	switch (typeof value) {
+		case 'undefined':
+			value = "";
+			break;
+
+		case 'object':
+			value = JSON.stringify(value);
+	}
+
+	var string = String(value);
 	var containsdelimiter = string.indexOf(delimiter) >= 0;
 	var containsQuote = string.indexOf(quote) >= 0;
 	var containsLinebreak = string.indexOf('\r') >= 0 || string.indexOf('\n') >= 0;
